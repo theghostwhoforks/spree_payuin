d037347c0bf5da45b517dbb870fc8169d5d94818
vi <flawed.flint@gmail.com>
2012-09-17 18:29:20 +0530

    removed test controller class in favour for new redirect routes DSL.

--- app/controllers/test_gateway_controller.rb
+++
@@ -1,5 +0,0 @@
-class TestGatewayController < Spree::BaseController
-  def payment
-    redirect_to gateway_payuin_callback_path(params)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.append do
   match "/gateway/payuin/:id/callback" => 'payuin/gateway#callback', :as => :gateway_payuin_callback
-  match "/test_gateway/_payment" => "test_gateway#payment", :as => :test_gateway
+  match "/test_gateway/_payment/:id", :to => redirect {|params| "/gateway/payuin/#{params[:id]}/callback"}, :as => :test_gateway
 end
