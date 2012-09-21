91bdf544c8d6083d8de9ad2c7c1bd4b65dc2e5c8
vi <flawed.flint@gmail.com>
2012-09-21 10:17:38 +0530

    addedpost  restriction for callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.append do
-  match "checkout/gateway/payuin/:id/callback" => 'checkout#callback', :as => :gateway_payuin_callback
+  match "checkout/gateway/payuin/:id/callback" => 'checkout#callback', :as => :gateway_payuin_callback, :method => :post
   match "/test_gateway/_payment/:id", :to => redirect {|params| "checkout/gateway/payuin/#{params[:id]}/callback"}, :as => :test_gateway
 end
