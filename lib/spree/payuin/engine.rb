2adffb0a5c7c896bd859dfd7bfd24f5a7364b1fa
vi <flawed.flint@gmail.com>
2012-10-15 14:28:05 +0530

    tweaking a few settings for rspec.

--- lib/spree/payuin/engine.rb
+++ lib/spree/payuin/engine.rb
@@ -5,6 +5,10 @@ module Spree
 
       config.autoload_paths += %W(#{config.root}/lib)
 
+      config.generators do |g|
+         g.test_framework :rspec
+       end
+
       def self.activate
         Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
           Rails.configuration.cache_classes ? require(c) : load(c)
