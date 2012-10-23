d637266e21ef920d67374f98eb3acc33becc639f
vi <flawed.flint@gmail.com>
2012-10-23 14:37:18 +0530

    few tentative security fixes to ensure that order cannot be updated when it is in external payment flow

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,13 +2,14 @@ Spree::CheckoutController.class_eval do
   include Spree::Payuin::Gateway
 
   def before_external_payment
-    @order.payments.destroy_all
+    order = Spree::Order.find_by_id(session[:order_id], :include => :adjustments)
+    order.payments.destroy_all
     payment_method = Spree::PaymentMethod.find_by_type "Spree::Payuin::PaymentMethod"
-    @order.payments.build(:amount => @order.total, :payment_method_id => payment_method.id)    
-    payment = Spree::Payuin::PaymentTransaction.build_using @order, payment_method
-    @order.payment.source = payment
-    @order.state = "external_payment"
-    @order.save!
+    order.payments.build(:amount => order.total, :payment_method_id => payment_method.id)
+    payment = Spree::Payuin::PaymentTransaction.build_using order, payment_method
+    order.payment.source = payment
+    order.state = "external_payment"
+    order.save!
   end
 
   def before_complete
--- app/controllers/spree/payuin/security_inflections.rb
+++ app/controllers/spree/payuin/security_inflections.rb
@@ -8,31 +8,35 @@ module Spree
       end
 
       def prevent_add_to_cart
-        order = current_order
-        return true unless order
-        if order.state == 'external_payment'
-          flash[:notice] = t(:duplicate_order)
-          redirect_to spree.cart_path 
+        if session[:order_id]
+          order = Spree::Order.find_by_id(session[:order_id])
+          return true unless order
+          if order.state == 'external_payment'
+            flash[:notice] = t(:duplicate_order)
+            redirect_to spree.cart_path 
+          end
         end
       end
 
-      #TODO - remove unsetting user on orde. we won't be able to track cart abandonment because of this
+      #TODO - remove unsetting user on order. we won't be able to track cart abandonment because of this
       def dissociate_user
         if user = try_spree_current_user
-          if order = current_order and order.state == 'external_payment'
-            @current_order = nil
-            session[:order_id] = nil
-            user.spree_orders.delete order
-            order.user_id = nil
-            order.state = 'cart'
-            order.save!
+          if session[:order_id]
+            if order = Spree::Order.find_by_id(session[:order_id]) and order.state == 'external_payment'
+              @current_order = nil
+              session[:order_id] = nil
+              user.spree_orders.delete order
+              order.user_id = nil
+              order.state = 'cart'
+              order.save!
+            end
           end
         end
       end
 
       def prevent_order_mutation_when_in_payment_mode
         if request.put?
-          order = current_order
+          order = Spree::Order.find_by_id(session[:order_id])
           if order.state == 'external_payment'
             flash[:notice] = t(:duplicate_order)
             redirect_to spree.cart_path 
